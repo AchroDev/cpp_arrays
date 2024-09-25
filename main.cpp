962647ccba4a4cd36df23f181fd99c5a7c558db8
AchroDev <achrodev@proton.me>
2024-09-25 00:12:37 -0500

    Added example code

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 int main()
 {
     int example[5]; // Example array of 5 integers
+    example[0] = 2; // Accessing and setting the first element of the array
+    example[4] = 4; // Accessing and setting the last element of the array
+
+    int a = example[0]; // You can set other variables to hold the value of any element in an array
+    // example[-1] = 5;    // Memory access violation, value cannot be less than the arrays first element
+    // example[5] = 2;     // Memory access violation, value cannot be more than the arrays last element
+
+    std::cout << example[0] << std::endl; // Prints the first index/element of the array
+    std::cout << example[4] << std::endl; // Prints the last index/element of the array
+    std::cout << example << std::endl;    // Prints the memory address of the array, 'example' here is actually just a pointer type
 
     std::cin.get();
 }
