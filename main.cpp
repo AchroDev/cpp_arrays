a7a00c05a9315f121ac3871580725a3ce83032f0
AchroDev <achrodev@proton.me>
2024-09-25 00:02:45 -0500

    Added comment and example array

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+// Arrays are "a collection of a bunch of variables" usually of the same type
+
 int main()
 {
+    int example[5]; // Example array of 5 integers
 
     std::cin.get();
 }
